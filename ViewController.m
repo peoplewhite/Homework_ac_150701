a4959e0beb3c3b1490b45b4750e01958352fd2ef
peoplewhite <shonshon7@gmail.com>
2015-07-01 15:03:49 +0800

    ready to add function

--- ViewController.m
+++ ViewController.m
@@ -21,5 +21,29 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+- (IBAction)btn1:(id)sender {
+}
+- (IBAction)btn2:(id)sender {
+}
+- (IBAction)btn3:(id)sender {
+}
+- (IBAction)btn4:(id)sender {
+}
+- (IBAction)btn5:(id)sender {
+}
+- (IBAction)btn6:(id)sender {
+}
+- (IBAction)btn7:(id)sender {
+}
+- (IBAction)btn8:(id)sender {
+}
+- (IBAction)btn9:(id)sender {
+}
+- (IBAction)btn10:(id)sender {
+}
+- (IBAction)btn11:(id)sender {
+}
+- (IBAction)btn12:(id)sender {
+}
 
 @end
